066581e627deb521548d976ca2f2152b38544345
pochopablo1 <pablo.santilli7@gmail.com>
2023-12-11 18:05:26 +0100

    streamlit changes

--- app.py
+++ app.py
@@ -3,47 +3,68 @@ import cv2
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import load_model
-import matplotlib.pyplot as plt
 
-# Title of the application
+# Configurar el estilo de la aplicación
+st.markdown(
+    """
+    <style>
+        body {
+            background-color: #000000;
+            color: #FFFFFF;
+        }
+        .main {
+            background-color: #000000;
+        }
+        .st-bd {
+            padding: 10px;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Título y descripción
 st.title('Image Classifier: Sad or Happy Person.')
+st.write("This simple application classifies whether a person in an uploaded image looks happy or sad.")
 
-# Load the model
+# Cargar el modelo
 model_path = 'models/happysadmodel.h5'
 model = load_model(model_path)
 
-# Widget to upload an image
+# Widget para cargar una imagen
 upload_file = st.file_uploader("Upload image file (jpg)", type=["jpg"])
 
-# Function to predict and display the image
+# Función para predecir y mostrar la imagen
 def predict(image):
-    # Check if the image is loaded successfully
+    # Verificar si la imagen se cargó correctamente
     if image is not None:
-        # Show the original image
+        # Mostrar la imagen original
         st.image(image, caption="Original Image", use_column_width=True)
 
-        # Resize the image
+        # Cambiar el tamaño de la imagen
         resize = tf.image.resize(image, (256, 256))
 
-        # Normalize and expand dimensions for prediction
+        # Normalizar y expandir dimensiones para la predicción
         input_image = np.expand_dims(resize / 255, 0)
 
-        # Make the prediction
+        # Hacer la predicción
         yhat = model.predict(input_image)
 
-        # Print the result
+        # Imprimir el resultado
         if yhat[0] > 0.5:
             result = "HAPPY"
         else:
             result = "SAD"
 
+        # Mostrar el resultado de la predicción con un título
+        st.write("## Prediction Result")
         st.write(f"Prediction: {result}")
     else:
-        st.write("Error: Unable to load the image.")
+        st.error("Please upload a valid image.")
 
-# Button to make the prediction
+# Botón para hacer la predicción
 if upload_file is not None:
-    # Read the image using OpenCV
+    # Leer la imagen usando OpenCV
     content = upload_file.getvalue()
     image = cv2.imdecode(np.frombuffer(content, np.uint8), 1)
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
